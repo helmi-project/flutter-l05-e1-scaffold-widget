2c23848be8858923c04fe8e7639b80e09dc5d3fc
helmibelogix <helmi@belogix.com>
2020-11-05 13:55:57 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/androidatc/l05e1scaffoldwidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.androidatc.l05e1scaffoldwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+void main() {
+  runApp(
+    //Semua yang diakhiri tanda : adalah atribut, seperti home: adalah atribut dari MaterialApp
+    MaterialApp(
+      //Widget yang membungkus sejumlah widget yang mengimplementasikan Material Design seperti Scaffold
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        //Route yang ditampilkan pertama saat aplikasi dijalankan
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+          backgroundColor:
+              Colors.green[500], //Warna bisa diberi gradient dari 50-900
+        ),
+        body: Center(
+          //Menjajarkan objek di tengah. Dijadikan parent karena diisi dengan widget lain
+          child: Text(
+            //Menampilkan dan menata text
+            'Hello, world!',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      ),
+    ),
+  );
+}
